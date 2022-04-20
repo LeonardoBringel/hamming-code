1a312c0210754dec79aed7a7dba7c199183f18b2
LeonardoBringel <leo.sbringel@gmail.com>
2022-04-20 11:04:04 -0300

    :sparkles: Implement hamming decode

--- hamming_code.py
+++ hamming_code.py
@@ -5,7 +5,6 @@ def parity_index(bits):
     while bit_index <= len(bits):
         parity_location.append(bit_index - 1)
         bit_index = bit_index * 2
-
     return parity_location
 
 
@@ -24,7 +23,6 @@ def parity(bits, interator):
                 cicle = interator
             else:
                 next_bit += 1
-
     return 0 if result % 2 == 0 else 1
 
 
@@ -34,5 +32,27 @@ def hamming_code(bits):
 
     for bit_index in parity_index(bits):
         bits[bit_index] = parity(bits, bit_index + 1)
-
     return bits
+
+
+def integrity_check(bits):
+    check_bits = bits.copy()
+
+    for bit_index in parity_index(check_bits):
+        check_bits[bit_index] = 0
+
+    for bit_index in parity_index(check_bits):
+        check_bits[bit_index] = parity(check_bits, bit_index + 1)
+    return check_bits == bits
+
+
+def hamming_decode(bits):
+    result = []
+
+    if integrity_check(bits):
+        for index, bit in enumerate(bits):
+            if index not in parity_index(bits):
+                result.append(bit)
+    else:
+        return [0]
+    return result
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from hamming_code import *
 
+print('-- Hamming Code --')
 bits = input('Enter the data bits: ')
 bits = [int(bit) for bit in bits]
 
 result = hamming_code(bits)
-
 result = ''.join(str(bit) for bit in result)
+
 print(f'Generated Hamming Code: {result}')
+
+print('\n-- Hamming Decode --')
+bits = input('Enter the data bits: ')
+bits = [int(bit) for bit in bits]
+
+result = hamming_decode(bits)
+result = ''.join(str(bit) for bit in result)
+print(f'Decoded Data: {result}')
