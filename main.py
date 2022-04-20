44f7a4bfbe36f8058c7cb0be1ec1c7da5fcae720
LeonardoBringel <leo.sbringel@gmail.com>
2022-04-19 23:30:35 -0300

    :sparkles: Implement hamming code

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Hello World!')
+from hamming_code import *
+
+bits = input('Enter the data bits: ')
+bits = [int(bit) for bit in bits]
+
+result = hamming_code(bits)
+
+result = ''.join(str(bit) for bit in result)
+print(f'Generated Hamming Code: {result}')
