dbd3017e5981624ee68b55b7b424166b9363f227
LeonardoBringel <leo.sbringel@gmail.com>
2022-04-23 12:45:10 -0300

    :lipstick: Implement simple menu to improve usability

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 from hamming_code import *
 
-print('-- Hamming Code --')
-bits = input('Enter the data bits: ')
-bits = [int(bit) for bit in bits]
+print('  _   _                                            ____          _      ')
+print(' | | | | __ _ _ __ ___  _ __ ___ (_)_ __   __ _   / ___|___   __| | ___ ')
+print(' | |_| |/ _` | \'_ ` _ \\| \'_ ` _ \\| | \'_ \\ / _` | | |   / _ \\ / _` |/ _ \\')
+print(' |  _  | (_| | | | | | | | | | | | | | | | (_| | | |__| (_) | (_| |  __/')
+print(' |_| |_|\\__,_|_| |_| |_|_| |_| |_|_|_| |_|\\__, |  \\____\\___/ \\__,_|\\___|')
+print('                                          |___/                         ')
 
-result = hamming_code(bits)
-result = ''.join(str(bit) for bit in result)
+while True:
+    answer = input('[1] - Code\t\t[2] - Decode\n[0] - Exit\t\tOption: ')
+    match answer:
+        case '1':
+            print('\n-- Hamming Code --')
 
-print(f'Generated Hamming Code: {result}')
+            bits = input('Enter the data bits: ')
+            bits = [int(bit) for bit in bits]
 
-print('\n-- Hamming Decode --')
-bits = input('Enter the data bits: ')
-bits = [int(bit) for bit in bits]
+            result = hamming_code(bits)
+            result = ''.join(str(bit) for bit in result)
+            print(f'Generated Hamming Code: {result}')
 
-result = hamming_decode(bits)
-result = ''.join(str(bit) for bit in result)
-print(f'Decoded Data: {result}')
+        case '2':
+            print('\n-- Hamming Decode --')
+
+            bits = input('Enter the data bits: ')
+            bits = [int(bit) for bit in bits]
+
+            result = hamming_decode(bits)
+            result = ''.join(str(bit) for bit in result)
+            print(f'Decoded Data: {result}')
+
+        case '0':
+            exit()
+    print('\n')
