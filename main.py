8c99e9ed156f65890086fd97551049d630142932
LeonardoBringel <leo.sbringel@gmail.com>
2022-06-28 02:07:01 -0300

    :sparkles: Implement window

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 from hamming import *
 from tkinter import *
+
+window = Tk()
+window.title('Hamming Code')
+window.resizable(False, False)
+window.eval('tk::PlaceWindow . center')
+
+canvas = Canvas(window, width=400, height=300, relief='raised')
+canvas.pack()
+
+label = Label(window, text='Hamming Code', font=('Bahnschrift', 16, 'bold', 'underline'))
+canvas.create_window(200, 25, window=label)
+
+window.mainloop()
