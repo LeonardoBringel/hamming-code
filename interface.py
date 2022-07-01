6a2fc5d5b969de35be0af1cc0a0242bf42addf8e
LeonardoBringel <leo.sbringel@gmail.com>
2022-07-01 15:27:48 -0300

    :truck: Move window class to interface.py

---
+++ interface.py
@@ -0,0 +1,66 @@
+from tkinter import Canvas, StringVar, Tk
+
+from hamming import *
+from interface_utils import *
+
+
+class Window(Tk):
+    def __init__(self):
+        super().__init__()
+        self.title('Hamming Code')
+        self.resizable(False, False)
+        self.eval('tk::PlaceWindow . center')
+
+        self.canvas = Canvas(self, width=400, height=300, relief='raised')
+        self.canvas.pack()
+
+        self.input_text = StringVar()
+        self.input_text.trace('w', self.button_state_controller)
+
+        self.label_text = StringVar()
+        self.output_text = StringVar()
+
+        validate_command = (self.register(self.validate_input), '%P')
+
+        create_label(self.canvas, 'Hamming Code', ('Bahnschrift', 16, 'bold', 'underline'), 200, 25)
+
+        create_label(self.canvas, 'Enter the data bits: ', ('Bahnschrift', 14), 200, 90)
+        self.entry_input = create_entry(self.canvas, self.input_text, ('Bahnschrift', 14), 200, 130, validate_command)
+
+        self.button_encode = create_button(self.canvas, 'Encode', self.get_hamming_code,
+                                           ('bahnschrift', 12, 'bold'), 160, 170)
+        self.button_decode = create_button(self.canvas, 'Decode', self.get_hamming_decode,
+                                           ('bahnschrift', 12, 'bold'), 240, 170)
+
+        create_label(self.canvas, self.label_text, ('Bahnschrift', 14), 200, 210)
+        self.entry_output = create_entry(self.canvas, self.output_text, ('Bahnschrift', 14), 200, 250)
+
+    def get_hamming_code(self):
+        self.label_text.set('Generated data: ')
+        self.get_answer(True)
+
+    def get_hamming_decode(self):
+        self.label_text.set('Decoded data: ')
+        self.get_answer(False)
+
+    def get_answer(self, encode=True):
+        bits = self.entry_input.get()
+        bits = [int(bit) for bit in bits]
+
+        result = hamming_code(bits) if encode else hamming_decode(bits)
+        result = ''.join(str(bit) for bit in result)
+        self.output_text.set(result)
+
+    @staticmethod
+    def validate_input(value):
+        if len(value) != 0:
+            value = value[-1]
+        return True if value == '1' or value == '0' or value == '' else False
+
+    def button_state_controller(self, *_):
+        if len(self.input_text.get()) > 0:
+            self.button_encode.config(state='normal')
+            self.button_decode.config(state='normal')
+        else:
+            self.button_encode.config(state='disabled')
+            self.button_decode.config(state='disabled')
